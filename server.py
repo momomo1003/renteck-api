2979712d85565b8dcf7a45844fd37914c9c4cea6
johanmallqui <johanmmalqui@gmail.com>
2024-01-15 14:59:06 +0900

    stuff was added

--- server.py
+++ server.py
@@ -1,10 +1,44 @@
+from typing import Optional
 import pyodbc
+import flask
 
-SERVER = "tcp:renteck-server.database.windows.net"
-DATABASE = "renteckDB"
-USERNAME = "renteck"
-PASSWORD = "SA_Renteck"
 
-conn_string = f"DRI:VER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}"
-conn = pyodbc.connect(conn_string)
-print("connexion success")
+class Database:
+    SERVER = "tcp:renteck-server.database.windows.net"
+    DATABASE = "renteckDB"
+    USERNAME = "renteck"
+    PASSWORD = "SA_Server"
+
+    def __init__(self) -> None:
+        self.conn: Optional[pyodbc.Connection] = None
+        self.cursor: Optional[pyodbc.Cursor] = None
+
+    def get_conn_str(self):
+        return f"DRI:VER={{ODBC Driver 18 for SQL Server}};SERVER={self.SERVER};DATABASE={self.DATABASE};UID={self.USERNAME};PWD={self.PASSWORD}"
+
+    def establish_conn(self):
+        self.conn = pyodbc.connect(self.get_conn_str())
+        self.cursor = self.conn.cursor()
+
+    def exec_query(self, sql: str):
+        if self.cursor:
+            self.cursor.execute(sql)
+
+
+app = flask.Flask(__name__)
+db = Database()
+
+
+@app.route("/list")
+def get_list():
+    sql = """SELECT FROM rental_management"""
+
+
+@app.route("/available")
+def get_available():
+    ...
+
+
+@app.route("/number")
+def get_number():
+    ...
