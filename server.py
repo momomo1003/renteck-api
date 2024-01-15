bb8591b684930010b33e033fd4ba2b0b3d53ae9e
root <johanmmalqui@gmail.com>
2024-01-15 05:14:32 +0000

    database connection success

--- server.py
+++ server.py
@@ -3,8 +3,23 @@ import pyodbc
 SERVER = "tcp:renteck-server.database.windows.net"
 DATABASE = "renteckDB"
 USERNAME = "renteck"
-PASSWORD = "SA_Renteck"
+PASSWORD = "SA_Server"
 
-conn_string = f"DRI:VER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}"
+conn_string = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}"
 conn = pyodbc.connect(conn_string)
-print("connexion success")
+if conn:
+    print("connexion success")
+else:
+    print("Could not connect")
+
+cursor = conn.cursor()
+QUERY = """INSERT INTO rental_management(rental_number, pc_number, id, startdata, enddata, received, returned) VALUES (1, 1, 'laptop', '20200606 10:10:20 AM', '20210606 10:20:30',0,0)"""
+cursor.execute(QUERY)
+QUERY = """SELECT pc_number, id FROM rental_management"""
+
+cursor.execute(QUERY)
+
+re = cursor.fetchall()
+
+for r in re:
+    print(r)
